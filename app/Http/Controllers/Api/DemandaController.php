ed557c119fc3bcb9573995e4571eab43959bb021
nakani <diogo.guedes.pereira@gmail.com>
2018-02-05 17:35:44 -0200

    ajuste

--- app/Http/Controllers/Api/DemandaController.php
+++ app/Http/Controllers/Api/DemandaController.php
@@ -18,13 +18,24 @@ class DemandaController extends Controller
 	public function GetDemandas(Request $request){
 		$id = Utils::getIdUser($request->header('Authorization'));
 
-		$resp = (array) DB::select("SELECT * FROM demands as d
+		$resp = (array) DB::select("SELECT d.id as id,
+											u.id as iduser,
+											u.name as nome,
+											d.name as titulodemanda,
+											d.description as descricaodemanda,
+											s.name as estado,
+											c.name as cidade,
+											d.created_at,
+											u.total_rating,
+											u.image											
+											 FROM demands as d
 									join users as u ON u.id = d.user_id
 									join states as s on s.id = d.state_id
 									join cities as c on c.id = d.city_id
-									where d.user_id<>'351'
+									where d.user_id<>$id
 									and d.executor_id is null
-									and u.id not in (select executor_id from demand_executor as de where de.executor_id='351') ");
+									and u.id not in (select executor_id from demand_executor as de where de.executor_id=$id) ");
+
 		return json_encode($resp);
 
 	}
