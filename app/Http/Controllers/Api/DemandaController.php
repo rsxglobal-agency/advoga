d088a3fffc1f709e095ac97c47d0b235666afc28
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-03-10 16:54:52 -0300

    ajuste sendDemand
     rev

--- app/Http/Controllers/Api/DemandaController.php
+++ app/Http/Controllers/Api/DemandaController.php
@@ -114,37 +114,31 @@ class DemandaController extends Controller
 	}
 
 	public static function sendDemand(Request $request){
-		try {
-			$user_id = Utils::getIdUser($request->header('Authorization'));
-		    
-		    $demand = new Demand;
-			
-			$demand->name = $request['name'];
-			$demand->description= $request['description'];
-			$demand->state_id = $request['state_id'];
-			$demand->city_id = $request['city_id'];	
-			$demand->user_id = $user_id;
-			$demand->ended = 0;
-			$resp = $demand->save();
-			if ($resp){
-				foreach ($request['atuations'] as $value) {
-					
-					
-				}
+		$user_id = Utils::getIdUser($request->header('Authorization'));
 
-				foreach ($request['services'] as $value) {
+		$demand = new Demand;
 
+		$demand->name = $request['name'];
+		$demand->description= $request['description'];
+		$demand->state_id = $request['state_id'];
+		$demand->city_id = $request['city_id'];	
+		$demand->user_id = $user_id;
+		$demand->ended = 0;
+		$resp = $demand->save();
 
-				}
-
-				return json_encode(Array('success' => true));
+		if ($resp) {
+			foreach ($request['atuations'] as $value) {
+				DB::insert('insert into atuation_demand (atuation_id, demand_id) values(?, ?)', [$value, $demand->id]);
 			}
-		} catch (Exception $error) {
-			error_log('exception: ');
-			error_log($error);
-		} finally {
-			return json_encode(Array('success' => false));
+
+			foreach ($request['services'] as $value) {
+				DB::insert('insert into demand_service (service_id, demand_id) values(?, ?)', [$value, $demand->id]);
+			}
+
+			return json_encode(Array('success' => true));
 		}
+
+		return json_encode(Array('success' => false));
 	}
 
 	public static function editDemand(Request $request) {
