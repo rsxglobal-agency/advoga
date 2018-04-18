36aa01467a0958ca04607f387c6bc4cea18ea4e2
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-04-18 17:31:52 -0300

    rev

--- app/Http/Controllers/Api/UtilsController.php
+++ app/Http/Controllers/Api/UtilsController.php
@@ -18,9 +18,40 @@ use App\Formation;
 use App\Titulation;
 use App\Demand;
 use Intervention\Image\ImageManagerStatic as Image;
-
-
 use App\AppResult;
+
+require __DIR__.'/vendor/autoload.php';
+
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+// This assumes that you have placed the Firebase credentials in the same directory
+// as this PHP file.
+$serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'../../../../advogaapp-firebase.json');
+
+$firebase = (new Factory)
+    ->withServiceAccount($serviceAccount)
+    // The following line is optional if the project id in your credentials file
+    // is identical to the subdomain of your Firebase project. If you need it,
+    // make sure to replace the URL with the URL of your project.
+    ->withDatabaseUri('https://advogaapp.firebaseio.com/')
+    ->create();
+
+$database = $firebase->getDatabase();
+
+$newPost = $database
+    ->getReference('blog/posts')
+    ->push([
+        'title' => 'Post title',
+        'body' => 'This should probably be longer.'
+    ]);
+
+$newPost->getKey(); // => -KVr5eu8gcTv7_AHb-3-
+$newPost->getUri(); // => https://my-project.firebaseio.com/blog/posts/-KVr5eu8gcTv7_AHb-3-
+
+$newPost->getChild('title')->set('Changed post title');
+$newPost->getValue(); // Fetches the data from the realtime database
+$newPost->remove();
 
 class UtilsController extends Controller
 {
