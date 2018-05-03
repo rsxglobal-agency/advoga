8b7df8485a0b2004267c37788226ef05aa43941a
lucasreis1994 <greislucas@hotmail.com>
2018-05-03 09:51:08 -0300

    teste Lucas Chat

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -47,7 +47,7 @@ class ChatController extends Controller
       // $selectUsuarios = $database->getReference('chat/users/'.$user->id.'/225/-LAJX_b_hf3JOvjPmBll');
       $usuariosChat = $selectUsuarios->getValue();
       foreach ($usuariosChat as $testeChat) {
-        echo $testeChat."<br>";
+        print_r($testeChat);
       }
       // print_r($usuariosChat);
       die;
